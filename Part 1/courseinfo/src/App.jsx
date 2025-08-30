868a724704b7faf2f1ff944a9c9eb9b0f410b017
Enijeshiku <eenijeshiku@gmail.com>
2025-08-30 05:33:17 +0100

    refactor: use Part components inside Content (step 1.2)

--- Part 1/courseinfo/src/App.jsx
+++ Part 1/courseinfo/src/App.jsx
@@ -7,19 +7,48 @@ const App = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
+  //Spliting the logic into 3 new components: Header, Content, and Total.
+
+  const Header = ({ course }) => {
+    return<h1>{course}</h1>
+  }
+  
+  const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
+    return (
+      <div>
+       <Part name={part1} exercises={exercises1}/>
+       <Part name={part2} exercises={exercises2}/>
+       <Part name={part3} exercises={exercises3}/>
+      </div>
+    )
+  }
+  
+  const Part = ({ name, exercises}) => {
+    return (
+      <p>{ name } {exercises}</p>
+    )
+  }
+  
+  const Total = ({ exercises1, exercises2, exercises3 }) => {
+    return(
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    )
+  }
+
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}/>
+      <Content 
+      part1={part1} exercises1={exercises1}
+      part2={part2} exercises2={exercises2}
+      part3={part3} exercises3={exercises3}
+
+      />
+      <Total 
+      exercises1={exercises1}
+      exercises2={exercises2}
+      exercises3={exercises3}
+      />
     </div>
   )
 }
